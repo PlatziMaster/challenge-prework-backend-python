a4a366d5f5256c89af7fc4c418bb5357f297f178
AlanVazquez99 <alanvazquez1999@gmail.com>
2020-08-13 23:56:47 -0500

    Factoring password validation function

--- src/main.py
+++ src/main.py
@@ -1,44 +1,28 @@
 # Resolve the problem!!
 import string
 
-SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
-
 
 def generate_password():
     # Start coding here
+    pass
+
+
+def validate_chars(chars_set, word):
+    for char in word:
+        if char in chars_set:
+            return True
 
 
 def validate(password):
-
     if len(password) >= 8 and len(password) <= 16:
-        has_lowercase_letters = False
-        has_numbers = False
-        has_uppercase_letters = False
-        has_symbols = False
 
-        for char in password:
-            if char in string.ascii_lowercase:
-                has_lowercase_letters = True
-                break
+        has_lowercase = validate_chars(string.ascii_lowercase, password)
+        has_uppercase = validate_chars(string.ascii_uppercase, password)
+        has_numbers = validate_chars(string.digits, password)
+        has_symbols = validate_chars(string.punctuation, password)
 
-        for char in password:
-            if char in string.ascii_uppercase:
-                has_uppercase_letters = True
-                break
-
-        for char in password:
-            if char in string.digits:
-                has_numbers = True
-                break
-
-        for char in password:
-            if char in SYMBOLS:
-                has_symbols = True
-                break
-
-        if has_symbols and has_numbers and has_lowercase_letters and has_uppercase_letters:
-            return True
-    return False
+        if has_symbols and has_numbers and has_lowercase and has_uppercase:
+            return password
 
 
 def run():
