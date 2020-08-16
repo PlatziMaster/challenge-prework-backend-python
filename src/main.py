45752b0a3939824d842d71e6e50fc8f5bbad8bd0
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-08-15 20:49:39 -0500

    Challenge completed

--- src/main.py
+++ src/main.py
@@ -1,11 +1,45 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
 
 
 def generate_password():
-    # Start coding here
+    # Obtener un número entre el 8 y 16
+    characters_in_password = random.randint(8, 16);
+    # Crear una lista de caracteres bajo los siguientes criterios
+    
+    # 2 números entre 0 y 30 (lista de símbolos) => 2 caracteres de la lista
+    array_index_simbols = [random.randint(0, 27), random.randint(0, 27)]
+    # 2 números entre 48 y 57 (ascii números) => 2 caracteres ascii
+    array_index_ascii_numbers = [random.randint(48, 57), random.randint(48, 57)]
+    # 2 números entre 65 y 90 (ascii letras mayúsculas) => 2 caracteres ascii
+    array_index_ascii_upper_characters = [random.randint(65, 90), random.randint(65, 90)]
+    # 2 números entre 97 y 122 (ascii letras minúsculas) => 2 caracteres ascii
+    array_index_ascii_lower_characters = [random.randint(97, 122), random.randint(97, 122)]    
+        
+    # Obtener string    
+    simbols_string = SYMBOLS[array_index_simbols[0]] + SYMBOLS[array_index_simbols[1]]
+    numbers_string = chr(array_index_ascii_numbers[0]) + chr(array_index_ascii_numbers[1])
+    upper_characters_string = chr(array_index_ascii_upper_characters[0]) + chr(array_index_ascii_upper_characters[1])
+    lower_characters_string = chr(array_index_ascii_lower_characters[0]) + chr(array_index_ascii_lower_characters[1])
+
+    # Verificar si no faltaron caracteres extra
+    extra_simbols_string = ""   
+    if characters_in_password > 8:
+        # Si faltaron, añadir la cantidad restante de los símbolos
+        length = characters_in_password - 8
+        for x in range(length):
+            symbol_index = random.randint(0,27)
+            extra_simbols_string += SYMBOLS[symbol_index]
+    
+    # Crear un string aleatorio con los caracteres obtenidos
+    password = simbols_string + numbers_string + upper_characters_string + lower_characters_string + extra_simbols_string
+    shuffle_password = "".join(random.sample(password, len(password)))    
+    
+    print(shuffle_password)
+    return shuffle_password
 
 
 def validate(password):
