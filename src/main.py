9fcc7a0fbcf3636fee9dd90823f47cea86ac29ba
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-08-16 22:25:25 -0500

    challenge password

--- src/main.py
+++ src/main.py
@@ -1,16 +1,30 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
 
-
 def generate_password():
     # Start coding here
+    letters_min = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+    letters_may = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z']
+    numbers = ['1','2', '3','4','5','6','7','8','9','0']
+    safe_password = letters_min + letters_may + numbers + SYMBOLS
+
+    final_password = []
+
+    for i in range(15):
+        generate_caracter = random.choice(safe_password)
+        final_password.append(generate_caracter)
+
+    final_password = "".join(final_password)
+    print(final_password)
+    return final_password
 
 
 def validate(password):
 
-    if len(password) >= 8 and len(password) <= 16:
+      if len(password) >= 8 and len(password) <= 16:
         has_lowercase_letters = False
         has_numbers = False
         has_uppercase_letters = False
@@ -38,7 +52,7 @@ def validate(password):
 
         if has_symbols and has_numbers and has_lowercase_letters and has_uppercase_letters:
             return True
-    return False
+        return False
 
 
 def run():
