9c5d2e44bfcd09ad43c5a4ef3bbe015d06856ecc
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2020-08-21 21:33:22 -0500

    Generate a random password

--- src/main.py
+++ src/main.py
@@ -9,25 +9,63 @@ def int_random_number(min, max):
     value = random.randint(min, max)
     return value
 
+def become_type_to_ascii(digit_type):
+    if digit_type == 1:
+        rand_symbol = int_random_number(1, 6)
+        return rand_symbol + 20
+    elif digit_type == 2:
+        rand_number = int_random_number(1, 9)
+        return rand_number + 30
+    elif digit_type == 3:
+        rand_upper = int_random_number(1, 9)
+        return rand_upper + 40
+    else:
+        rand_lower = int_random_number(1, 9)
+        return rand_lower + 60
+
+def password_hex_to_ascii(hex_string):
+    print('hex to ascii')
+    # hex_string = "616263"[0:]
+    # hex_string = hex_string[0:]
+    # print(hex_string)
+    bytes_object = bytes.fromhex(hex_string)
+    # print(bytes_object)
+    ascii_string = bytes_object.decode("ASCII")
+    # print(ascii_string)
+    # print(type(ascii_string))
+    # str_password = str(ascii_string, 'utf-8')
+    # print(type(str_password))
+    return ascii_string
+
+
 def generate_password():
     # Start coding here
     # 1.do
     #    1.1. Generate password length  (ok)
     #    1.2. for i = 0 to password_length (ok)
-    #       1.2.1 Generate digit type
-    #       1.2.2 Generate random digit
-    #       1.2.3 concat to password
+    #       1.2.1 Generate digit type (ok)
+    #       1.2.2 Generate random digit (ok)
+    #       1.2.3 Generate hex password  (ok)       
+    #       1.2.4 concat to password (ok)
+    #       1.2.5 hex to ascii
     #    1.3. Verify if password is secure
     #   while(password === insecure) 
     # 4. Return password
 
+    hex_password = ''
     password_length = int_random_number(8, 16)
     print(password_length)
-    for x in range(password_length):
-        type_digit = int_random_number(1, 4) 
-        print(type_digit)
 
-    password = 'PiPe(12)'
+    for x in range(password_length):
+        digit_type = int_random_number(1, 4) 
+        new_digit = become_type_to_ascii(digit_type)
+        str_digit = str(new_digit)
+        hex_password = hex_password + str_digit 
+
+    password = password_hex_to_ascii(hex_password)
+    print(password)
+    print(type(hex_password))
+    
     return password
 
 
@@ -66,6 +104,7 @@ def validate(password):
 
 def run():
     password = generate_password()
+    print(password)
     if validate(password):
         print('Secure Password')
     else:
